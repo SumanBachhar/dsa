18f3657db0090f5610746a299821227363134165
Suman-199 <suman.mmt199@gmail.com>
2024-06-05 19:43:49 +0530

    valid anagram complete

---
+++ day-3/validanagram.js
@@ -0,0 +1,16 @@
+const s = "rat";
+const t = "car";
+const isAnagram = (s, t) => {
+  str = s.split("").sort();
+  tst = t.split("").sort();
+
+  for (var i = 0; i < str.length; i++) {
+    if (str[i] !== tst[i]) {
+      console.log(false);
+    } else if (str.length !== tst.length) {
+      console.log(false);
+    }
+  }
+  console.log(true);
+};
+isAnagram(s, t);
