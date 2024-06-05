a51f6c8f1acdcc2434c5cbc95d3696017ade1c96
Suman-199 <suman.mmt199@gmail.com>
2024-06-05 17:46:31 +0530

    day 2 complete

---
+++ day-2/mergeTwoList.js
@@ -0,0 +1,14 @@
+const list1 = [1, 2, 4];
+const list2 = [1, 3, 4];
+var mergeTwoLists = function (list1, list2) {
+  if (!list1) return list2;
+  else if (!list2) return list1;
+  else if (list1.val <= list2.val) {
+    list1.next = mergeTwoLists(list1.next, list2);
+    return list1;
+  } else {
+    list2.next = mergeTwoLists(list1, list2.next);
+    return list2;
+  }
+};
+mergeTwoLists(list1, list2);
