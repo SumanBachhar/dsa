46f1e6778c42fff1e02111c3ceb430e8d3929fa3
Suman-199 <suman.mmt199@gmail.com>
2024-06-12 17:41:10 +0530

    find 2 complete

--- practice/findDuplicate.js
+++ practice/findDuplicate.js
@@ -1,8 +1,8 @@
-const nums = [1, 4, 5, "banana", 6, 1, 4, 4, 9, 3, "banana"];
+const nums = [3, 3, 3, 3, 3];
 
 const findDuplicate = (nums) => {
-  let sortedArray = nums.sort();
-  let results = [];
+  const sortedArray = nums.sort();
+  const results = [];
 
   for (let i = 0; i < sortedArray.length - 1; i++) {
     if (sortedArray[i + 1] === sortedArray[i]) {
@@ -12,3 +12,16 @@ const findDuplicate = (nums) => {
   console.log(results);
 };
 findDuplicate(nums);
+
+var findDup = function (nums) {
+  let obj = {};
+
+  for (let i of nums) {
+    if (!obj[i]) {
+      obj[i] = 1;
+    } else {
+      console.log(i);
+    }
+  }
+};
+findDup(nums);
--- practice/reverseString.js
+++ practice/reverseString.js
@@ -6,10 +6,21 @@ console.log(reverse);
 
 // reverse a string without method
 
-let str = "React is a javascript library";
-let reverseStr = "";
+// let str = "React is a javascript library";
+// let reverseStr = "";
 
-for (let i = str.length - 1; i >= 0; i--) {
-  reverseStr += str[i];
-}
-console.log(reverseStr);
+// for (let i = str.length - 1; i >= 0; i--) {
+//   reverseStr += str[i];
+// }
+// console.log(reverseStr);
+
+// const str = ["i", "like", "this", "program", "very", "much"];
+const str = "React is a javascript library";
+const reverseString = (str) => {
+  let reversedStr = "";
+  for (let i = str.length - 1; i >= 0; i--) {
+    reversedStr += str[i] + "";
+  }
+  console.log(reversedStr);
+};
+reverseString(str);
