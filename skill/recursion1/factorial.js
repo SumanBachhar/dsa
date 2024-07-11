56651ea8088e15e1c06d3e453e7e3f27e16f525b
Suman-199 <suman.mmt199@gmail.com>
2024-07-11 12:29:57 +0530

    fac and fibo

---
+++ skill/recursion1/factorial.js
@@ -0,0 +1,20 @@
+function factorialize(num) {
+  if (num === 0) {
+    return 1;
+  }
+  return num * factorialize(num - 1);
+}
+
+console.log(factorialize(5));
+
+const f = (n) => {
+  if (n === 0) {
+    return;
+  }
+  console.log(n);
+  f(n - 1);
+  console.log(n);
+
+  // console.log(n * f(n - 1));
+};
+f(4);
